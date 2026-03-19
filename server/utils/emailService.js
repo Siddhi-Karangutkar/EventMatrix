b9bdc972bff624aa1dc2dba75609ace609ef4657
Siddhi Karangutkar <karangutkarsiddhi19@gmail.com>
2026-03-19 19:14:51 +0530

    Switch to high-level gmail service abstraction

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -3,20 +3,14 @@ const nodemailer = require('nodemailer');
 const sendApprovalEmail = async (clubEmail, clubName, headName) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false, // Use STARTTLS
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
             tls: {
                 rejectUnauthorized: false
-            },
-            family: 4, 
-            connectionTimeout: 10000,
-            greetingTimeout: 10000,
-            socketTimeout: 10000
+            }
         });
 
         const mailOptions = {
